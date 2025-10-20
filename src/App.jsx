4b31cafd3afc2b3c43397ac5c1f8eb8d43ad5635
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-10-20 17:03:48 -0500

    feat: Setup routing and create initial layouts with Sidebar:
    - Configure React Router DOM for application routing.
    - Remove unused default files from Vite template.
    - Create base layout structure.
    - Implement initial Sidebar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClient } from "@tanstack/react-query";
-import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MyRoutes } from "./routers/router";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
+      <MyRoutes />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
