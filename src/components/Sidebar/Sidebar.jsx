7fc82e1b4d9a0aa73d3b17dcf70d9b31fa32b83f
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-10-21 21:12:45 -0500

    feat: The sidebar design process begins.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,67 @@
+import { Icon } from "@iconify/react";
+import { NavLink } from "react-router-dom";
+
 export const Sidebar = () => {
+  const links = [
+    {
+      label: "Inicio",
+      icon: "ic:baseline-home",
+      to: "/",
+    },
+    {
+      label: "Notificaciones",
+      icon: "ic:baseline-notifications",
+      to: "/notificaciones",
+    },
+    {
+      label: "Mensajes",
+      icon: "ic:baseline-message",
+      to: "/mensajes",
+    },
+    {
+      label: "Colecciones",
+      icon: "ic:baseline-collections-bookmark",
+      to: "/colecciones",
+    },
+    {
+      label: "Suscripciones",
+      icon: "ic:baseline-person",
+      to: "/suscripciones",
+    },
+    {
+      label: "Añadir tarjeta",
+      icon: "ic:baseline-credit-card",
+      to: "/tarjeta",
+    },
+    {
+      label: "Mi perfil",
+      icon: "ic:baseline-account-circle",
+      to: "/miperfil",
+    },
+  ];
   return (
-    <div className="h-screen bg-amber-300 text-black">
-      <span>Sidebar</span>
+    <div className="h-screen p-2 bg-white dark:bg-bg-dark transition-all duration-300">
+      <div>LINKER</div>
+      <nav className="flex-1 flex flex-col gap-2 items-center">
+        {links.map((item, index) => {
+          return (
+            <NavLink
+              key={index}
+              to={item.to}
+              className={({ isActive }) =>
+                `flex items-center gap-3 p-2 rounded-lg font-semibold hover:bg-gray-100 dark:hover:bg-primary/10 dark:hover:text-primary transition-all w-full justify-start ${
+                  isActive
+                    ? "text-blue-600 dark:text-white"
+                    : "text-gray-600 dark:text-gray-400"
+                }`
+              }
+            >
+              <Icon icon={item.icon} width={24} height={24} />
+              <span>{item.label}</span>
+            </NavLink>
+          );
+        })}
+      </nav>
     </div>
   );
 };
