0ba3aac28aa2b705ef895e73e60262ac021a799b
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-10-23 22:43:58 -0500

    feat: New buttons are created to log out and add a new post, and responsiveness is added to the sidebar.

---
+++ src/components/ui/buttons/BtnLogout.jsx
@@ -0,0 +1,10 @@
+import { Icon } from "@iconify/react";
+
+export const BtnLogout = () => {
+  return (
+    <div className="flex items-center gap-3 p-2 rounded-lg hover:bg-red-400 dark:hover:bg-primery/20 transition-all justify-start cursor-pointer">
+      <Icon icon={"solar:logout-2-bold-duotone"} width={24} height={24} />
+      <span className="hidden sm:block">Cerrar Sesión</span>
+    </div>
+  );
+};
--- src/components/ui/buttons/BtnToggleTheme.jsx
+++ src/components/ui/buttons/BtnToggleTheme.jsx
@@ -3,9 +3,14 @@ import { useThemeStore } from "../../../store/ThemeStore";
 export const BtnToggleTheme = () => {
   const { theme, setTheme } = useThemeStore();
   return (
-    <button className="flex items-center gap-3 p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-primary/20 transition-all justify-center cursor-pointer" onClick={setTheme}>
-      <span>{theme === "light" ? "🌞" : "🌙"}</span>
-      <span>{theme === "light" ? "Claro" : "Oscuro"}</span>
+    <button
+      className="flex items-center gap-3 p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-primary/20 transition-all justify-start cursor-pointer"
+      onClick={setTheme}
+    >
+      <span>{theme === "light" ? "🌞" : "🌚"}</span>
+      <span className="hidden sm:block">
+        {theme === "light" ? "Claro" : "Oscuro"}
+      </span>
     </button>
   );
 };
