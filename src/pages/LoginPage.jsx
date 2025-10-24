5996f3d5915ed86d8c77cab6837bd5bb57fd8187
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-10-24 17:53:57 -0500

    feat: We begin to layout the design of the login on the right side and the login form and design of the inputs.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,15 @@
+import { Icon } from "@iconify/react";
 import logo from "../assets/logo.png";
+import { useState } from "react";
 
 export const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <div className="flex h-screen w-full">
-      <section className="bg-[#00b0f0] flex flex-col justify-center items-center overflow-hidden">
+      <section className="hidden md:flex md:w-1/2 bg-[#00b0f0] flex-col justify-center items-center overflow-hidden">
         <div className="px-8 text-white text-center flex flex-col gap-4">
           <div className="flex  items-center gap-3 justify-center">
             <img src={logo} className="h-10 w-10" />
@@ -20,7 +26,35 @@ export const LoginPage = () => {
           </div>
         </div>
       </section>
-      <section></section>
+      <section className="w-full md:w-1/2 flex items-center justify-center px-6 md:px-16 py-8">
+        <div className="w-full max-w-md">
+          <h1 className="text-2xl font-medium mb-6 text-center">
+            Iniciar sesión{" "}
+            <span className="text-[#0091EA] text-xl">(modo invitado)</span>
+          </h1>
+          <form>
+            <div className="mb-4">
+              <input
+                placeholder="tucorreo@ejemplo.com"
+                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#00aff0]"
+              />
+            </div>
+            <div className="relative">
+              <input
+                placeholder="Ingresa la contraseña"
+                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#00aff0]"
+              />
+              <button
+                type="button"
+                className="absolute top-1/2 -translate-y-1/2 right-3 text-gray-500 cursor-pointer"
+                onClick={togglePasswordVisibility}
+              >
+                <Icon icon={showPassword ? "mdi:eye-off" : "mdi:eye"} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
     </div>
   );
 };
