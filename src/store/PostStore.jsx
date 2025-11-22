299ee6ebe997e6816100103db51c3deecace8495
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-11-22 17:23:13 -0500

    feat: Files are uploaded to Supabase storage.

--- src/store/PostStore.jsx
+++ src/store/PostStore.jsx
@@ -1,4 +1,50 @@
 import { create } from "zustand";
+import { supabase } from "../supabase/supabase.config";
+
+const tabla = "publicaciones";
+const InsertarPost = async (p) => {
+  const { data, error } = await supabase
+    .from(tabla)
+    .insert(p)
+    .select()
+    .maybeSingle();
+  if (error) {
+    throw new Error(error.message);
+  }
+  if (file) {
+    const nuevo_id = data?.id;
+    const urlImagen = await subirArchivo(nuevo_id, file);
+    const pUrl = {
+      url: urlImagen,
+      id: nuevo_id,
+    };
+    await EditarPublicacion(pUrl);
+  }
+};
+const EditarPublicacion = async (p) => {
+  const { error } = await supabase.from(tabla).update(p).eq("id", p.id);
+  if (error) {
+    throw new Error(error.message);
+  }
+};
+const subirArchivo = async (id, file) => {
+  const ruta = "publicaciones/" + id;
+  const { data, error } = await supabase.storage
+    .from("archivos")
+    .upload(ruta, file, {
+      cacheControl: "0",
+      upsert: true,
+    });
+  if (error) {
+    throw new Error(error.message);
+  }
+  if (data) {
+    const { data: urlimagen } = await supabase.storage
+      .from("archivos")
+      .getPublicUrl(ruta);
+    return urlimagen;
+  }
+};
 
 export const usePostStore = create((set) => ({
   file: null,
