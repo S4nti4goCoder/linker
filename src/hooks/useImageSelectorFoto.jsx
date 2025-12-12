2bd5e3a08bfd538b0856038b9c32b4bfc14747c7
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-12-11 21:11:36 -0500

    feat: It is created to be able to upload an image to the data update.

--- src/hooks/useImageSelectorFoto.jsx
+++ src/hooks/useImageSelectorFoto.jsx
@@ -1,6 +1,39 @@
 import { Icon } from "@iconify/react";
+import { useRef } from "react";
+import { toast } from "sonner";
+import imageCompression from "browser-image-compression";
+import { useGlobalStore } from "../store/GlobalStore";
 
-export const ImageSelectorFoto = ({ fileUrl = "-" }) => {
+export const ImageSelectorFoto = () => {
+  const { setFile, setFileUrl, fileUrl } = useGlobalStore();
+  const fileInputRef = useRef(null);
+  function openFileSelector() {
+    fileInputRef.current.click();
+  }
+  const handleImageChange = async (e) => {
+    const selectedFile = e.target.files[0];
+    if (!selectedFile) return;
+    if (!selectedFile.type.startsWith("image/")) {
+      alert("Solo se permiten imágenes.");
+      return;
+    }
+    try {
+      const options = {
+        maxSizeMB: selectedFile.size > 1024 * 1024 ? 0.1 : 0.2,
+        maxWidthOrHeight: 1920,
+        useWebWorker: true,
+      };
+      const compressedFile = await imageCompression(selectedFile, options);
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(compressedFile);
+      setFile(compressedFile);
+      fileReader.onload = () => {
+        setFileUrl(fileReader.result);
+      };
+    } catch (error) {
+      toast.error("Error al comprimir la imagen:", error);
+    }
+  };
   return (
     <div className="text-center mb-5">
       <div className="relative inline-block">
@@ -12,12 +45,18 @@ export const ImageSelectorFoto = ({ fileUrl = "-" }) => {
           className="w-20 h-20 rounded-lg object-cover transition-transform duration-300 hover:scale-105"
         />
         <button className="absolute top-2 left-14 w-7 h-7 bg-neutral-800 hover:bg-neutral-600 text-white rounded-full flex items-center justify-center transition-all duration-200 cursor-pointer">
-          <Icon icon="lets-icons:edit-fill" className="text-[18px]" />
+          <Icon
+            icon="lets-icons:edit-fill"
+            className="text-[18px]"
+            onClick={openFileSelector}
+          />
         </button>
         <input
+          ref={fileInputRef}
           accept="image/jpeg, image/png,/*"
           type="file"
           className="hidden"
+          onChange={handleImageChange}
         />
       </div>
     </div>
