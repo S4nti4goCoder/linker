dcb331cd5776dc7be2a3ec151c829c989942a32b
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2026-03-08 12:48:47 -0500

    fix(ui): fix typo primery -> primary in BtnLogout

--- src/components/ui/buttons/BtnLogout.jsx
+++ src/components/ui/buttons/BtnLogout.jsx
@@ -5,7 +5,7 @@ export const BtnLogout = () => {
   const { cerrarSesion } = useAuthStore();
   return (
     <div
-      className="flex items-center gap-3 p-2 rounded-lg hover:bg-red-400 dark:hover:bg-primery/20 transition-all justify-center sm:justify-start cursor-pointer"
+      className="flex items-center gap-3 p-2 rounded-lg hover:bg-red-400 dark:hover:bg-primary/20 transition-all justify-center sm:justify-start cursor-pointer"
       onClick={cerrarSesion}
     >
       <Icon icon={"solar:logout-2-bold-duotone"} width={24} height={24} />
