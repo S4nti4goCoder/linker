ffd60fbe05753cd1bdae19beb96b62d9826a2959
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2026-03-10 23:54:46 -0500

    fix: use UTC ISO string for post date to prevent timezone offset bug

--- src/hooks/useFormattedDate.jsx
+++ src/hooks/useFormattedDate.jsx
@@ -1,8 +1,3 @@
 export function useFormattedDate() {
-  const fechaActual = new Date();
-  const offset = fechaActual.getTimezoneOffset() * 60000;
-  return new Date(fechaActual - offset)
-    .toISOString()
-    .slice(0, 19)
-    .replace("T", " ");
+  return new Date().toISOString();
 }
